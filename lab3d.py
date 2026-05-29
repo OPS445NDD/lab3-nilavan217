b19819a4c513e50afaffd55359609b104b31e8b3
Nilavan Selvakumar <nilavan217@gmail.com>
2026-05-29 09:21:36 -0400

    Added lab3d

---
+++ lab3d.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+'''Lab 3 function free_space'''
+# Author ID: nselvakumar2
+
+import subprocess
+
+def free_space():
+    
+    p = subprocess.Popen("df -h | grep '/$' | awk '{print $4}'", stdout=subprocess.PIPE, stdin=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
+    
+    output = p.communicate()
+    
+    return output[0].decode('utf-8').strip()
+
+if __name__ == '__main__':
+    print(free_space())
